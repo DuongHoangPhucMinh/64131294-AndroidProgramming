2bf67e9c70eb7fe7f62b605d124208403ec387a6
DuongHoangPhucMinh <168085266+DuongHoangPhucMinh@users.noreply.github.com>
2025-02-26 22:58:44 +0700

    Ex4_AddSubMulDiv_Onclick

---
+++ Ex4_AddSubMulDiv_Onclick/app/src/main/java/minh/edu/vd2/MainActivity.java
@@ -0,0 +1,111 @@
+package minh.edu.vd2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editTextSo1;
+    EditText editTextSo2;
+    EditText editTextKQ;
+    Button nutCong,nutTru,nutNhan,nutChia;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        TimDieuKhien();
+    }
+
+    void TimDieuKhien(){
+        editTextSo1 = (EditText)findViewById(R.id.edtSo1);
+        editTextSo2 = (EditText)findViewById(R.id.edtSo2);
+        editTextKQ = (EditText) findViewById(R.id.edtKetQua);
+        nutCong = (Button) findViewById(R.id.btnCong);
+        nutTru = (Button) findViewById(R.id.btnTru);
+        nutNhan = (Button) findViewById(R.id.btnNhan);
+        nutChia = (Button) findViewById(R.id.btnChia);
+    }
+
+    void XuLyCong(View view){
+
+        String soThu1 = editTextSo1.getText().toString();
+        String soThu2 = editTextSo2.getText().toString();
+
+        float soA = Float.parseFloat(soThu1);
+        float soB = Float.parseFloat(soThu2);
+
+        float Tong = soA + soB;
+
+
+
+        String chuoiKQ = String.valueOf(Tong);
+
+        editTextKQ.setText(chuoiKQ);
+    }
+    void XuLyTru(View view){
+
+
+        String soThu1 = editTextSo1.getText().toString();
+        String soThu2 = editTextSo2.getText().toString();
+
+        float soA = Float.parseFloat(soThu1);
+        float soB = Float.parseFloat(soThu2);
+
+        float Tong = soA - soB;
+
+
+
+        String chuoiKQ = String.valueOf(Tong);
+
+        editTextKQ.setText(chuoiKQ);
+    }
+
+    void XuLyNhan(View view){
+
+
+        String soThu1 = editTextSo1.getText().toString();
+        String soThu2 = editTextSo2.getText().toString();
+
+        float soA = Float.parseFloat(soThu1);
+        float soB = Float.parseFloat(soThu2);
+
+        float Tong = soA * soB;
+
+
+
+        String chuoiKQ = String.valueOf(Tong);
+
+        editTextKQ.setText(chuoiKQ);
+    }
+
+    void XuLyChia(View view){
+
+        String soThu1 = editTextSo1.getText().toString();
+        String soThu2 = editTextSo2.getText().toString();
+
+        float soA = Float.parseFloat(soThu1);
+        float soB = Float.parseFloat(soThu2);
+
+        float Tong = soA / soB;
+
+
+
+        String chuoiKQ = String.valueOf(Tong);
+
+        editTextKQ.setText(chuoiKQ);
+    }
+
+}
